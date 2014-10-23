07b0360b1793c2cfdc32ae7d8c23976ee948ad97
Berkus Decker <berkus@madfire.net>
2014-10-24 01:10:02 +0300

    Add scalar wrappers to try and improve Swift arithmetic

---
+++ chess2/Easy-Cal.swift
@@ -0,0 +1,171 @@
+//
+//  Easy-Cal.swift
+//  tableview
+//
+//  Created by 王 巍 (@onevcat) on 14-6-4.
+//  Copyright (c) 2014年 OneV's Den. All rights reserved.
+//  https://github.com/onevcat/Easy-Cal-Swift
+//  MIT License
+//
+
+import Foundation
+
+func +(lhs: Int, rhs: Double) -> Double {
+    return Double(lhs) + rhs
+}
+
+func +(lhs: Double, rhs: Int) -> Double {
+    return lhs + Double(rhs)
+}
+
+func +(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) + rhs
+}
+
+func +(lhs: Float, rhs: Int) -> Float {
+    return lhs + Float(rhs)
+}
+
+func +(lhs: Float, rhs: Double) -> Double {
+    return Double(lhs) + rhs
+}
+
+func +(lhs: Double, rhs: Float) -> Double {
+    return lhs + Double(rhs)
+}
+
+func +(lhs: UInt, rhs: Double) -> Double {
+    return Double(lhs) + rhs
+}
+
+func +(lhs: Double, rhs: UInt) -> Double {
+    return lhs + Double(rhs)
+}
+
+func +(lhs: UInt, rhs: Float) -> Float {
+    return Float(lhs) + rhs
+}
+
+func +(lhs: Float, rhs: UInt) -> Float {
+    return lhs + Float(rhs)
+}
+
+func -(lhs: Int, rhs: Double) -> Double {
+    return Double(lhs) - rhs
+}
+
+func -(lhs: Double, rhs: Int) -> Double {
+    return lhs - Double(rhs)
+}
+
+func -(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) - rhs
+}
+
+func -(lhs: Float, rhs: Int) -> Float {
+    return lhs - Float(rhs)
+}
+
+func -(lhs: Float, rhs: Double) -> Double {
+    return Double(lhs) - rhs
+}
+
+func -(lhs: Double, rhs: Float) -> Double {
+    return lhs - Double(rhs)
+}
+
+func -(lhs: UInt, rhs: Double) -> Double {
+    return Double(lhs) - rhs
+}
+
+func -(lhs: Double, rhs: UInt) -> Double {
+    return lhs - Double(rhs)
+}
+
+func -(lhs: UInt, rhs: Float) -> Float {
+    return Float(lhs) - rhs
+}
+
+func -(lhs: Float, rhs: UInt) -> Float {
+    return lhs - Float(rhs)
+}
+
+func *(lhs: Int, rhs: Double) -> Double {
+    return Double(lhs) * rhs
+}
+
+func *(lhs: Double, rhs: Int) -> Double {
+    return lhs * Double(rhs)
+}
+
+func *(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) * rhs
+}
+
+func *(lhs: Float, rhs: Int) -> Float {
+    return lhs * Float(rhs)
+}
+
+func *(lhs: Float, rhs: Double) -> Double {
+    return Double(lhs) * rhs
+}
+
+func *(lhs: Double, rhs: Float) -> Double {
+    return lhs * Double(rhs)
+}
+
+func *(lhs: UInt, rhs: Double) -> Double {
+    return Double(lhs) * rhs
+}
+
+func *(lhs: Double, rhs: UInt) -> Double {
+    return lhs * Double(rhs)
+}
+
+func *(lhs: UInt, rhs: Float) -> Float {
+    return Float(lhs) * rhs
+}
+
+func *(lhs: Float, rhs: UInt) -> Float {
+    return lhs * Float(rhs)
+}
+
+func /(lhs: Int, rhs: Double) -> Double {
+    return Double(lhs) / rhs
+}
+
+func /(lhs: Double, rhs: Int) -> Double {
+    return lhs / Double(rhs)
+}
+
+func /(lhs: Int, rhs: Float) -> Float {
+    return Float(lhs) / rhs
+}
+
+func /(lhs: Float, rhs: Int) -> Float {
+    return lhs / Float(rhs)
+}
+
+func /(lhs: Float, rhs: Double) -> Double {
+    return Double(lhs) / rhs
+}
+
+func /(lhs: Double, rhs: Float) -> Double {
+    return lhs / Double(rhs)
+}
+
+func /(lhs: UInt, rhs: Double) -> Double {
+    return Double(lhs) / rhs
+}
+
+func /(lhs: Double, rhs: UInt) -> Double {
+    return lhs / Double(rhs)
+}
+
+func /(lhs: UInt, rhs: Float) -> Float {
+    return Float(lhs) / rhs
+}
+
+func /(lhs: Float, rhs: UInt) -> Float {
+    return lhs / Float(rhs)
+}
